af80ebb9adb063a2b1dd5c7ff6a54534c413864f
ubuntu <ubuntu@localhost.localdomain>
2019-01-18 18:54:24 +0800

    设置进程名称

--- main.c
+++ main.c
@@ -4,16 +4,32 @@
 #include <signal.h>
 #include <sys/wait.h>
 
-//void signal_handler(  );
+// 定义信号处理函数
+void signal_handler( int );
+// 定义处理进程名称的函数
+void set_process_title();
 
 // 子进程数量
 const int worker_num = 4;
 
 int main( int argc, char * argv[] ) {
+
+    
+  /*
+  for ( int i = 0; NULL != argv[ i ]; i++ ) {
+    printf( "%p : %s\n", argv[ i ], argv[ i ] );
+  }
+  */
+  //printf( "%p\n", argv[ 0 ] ); 
+  char * process_title = argv[ 0 ];
   
   pid_t pid;
   // 用child_pid数组用来保存子进程的进程id
   pid_t child_pid[ worker_num ];
+  // 声明sigaction-struct结构体变量
+  struct sigaction sa_struct;
+  sa_struct.sa_flags   = SA_RESTART;
+  sa_struct.sa_handler = signal_handler;
  
   // fork子进程
   for ( int i = 0; i < worker_num; i++ ) {
@@ -25,7 +41,7 @@ int main( int argc, char * argv[] ) {
     }
     // 在子进程中.
     else if ( 0 == pid ) {
-      sleep( 20 );
+      sleep( 10 );
       exit( -1 );
     }
     // 在父进程中.
@@ -35,19 +51,24 @@ int main( int argc, char * argv[] ) {
     }
   }
 
+  // 为主进程安装信号管理器
+  sigaction( SIGCHLD, &sa_struct, NULL );
+
   // 主进程要回收子进程，防止僵尸进程.
-  while ( 1 ) {
-    sleep( 1 );
-    for ( int i = 0; i < worker_num; i++ ) {
-      int wait_status_value;
-      waitpid( child_pid[ i ], &wait_status_value, WNOHANG );
+  //while ( 1 ) {
+    //sleep( 1 );
+    //for ( int i = 0; i < worker_num; i++ ) {
+      //int wait_status_value;
+      //waitpid( child_pid[ i ], &wait_status_value, WNOHANG );
       //if (WIFEXITED(stat_val))//正常退出
       //if (WIFSIGNALED(stat_val))//查看被什么信号关闭
-    }
+    //}
+  //}
+  //sleep( 1000000 );
+  while ( 1 ) {
+    sleep( 1 );
   }
 
-  // 为祝进程安装信号管理器
-  //sigaction(  );
   /*
   int worker_num_total = sizeof( child_pid ) / sizeof( pid_t );
   for ( int i = 0; i < worker_num_total ; i++ ) {
@@ -57,4 +78,11 @@ int main( int argc, char * argv[] ) {
   return 0;
 }
 
-//void sigaction() {}
+void signal_handler( int signal ) {
+  // 子进程exit时候，内核会给父进程发送SIGCHLD信号，主进程回收子进程
+  if ( SIGCHLD == signal ) {
+    int   waitpid_status;
+    pid_t pid;
+    pid = waitpid( -1, &waitpid_status, WNOHANG );   
+  }
+}
