bbe0d68f20dc6e3d2382ed76a48b247107545b87
elarity <42677833@qq.com>
2019-01-23 17:51:45 +0800

    添加daemon函数

--- main.c
+++ main.c
@@ -3,6 +3,9 @@
 #include <unistd.h>
 #include <signal.h>
 #include <string.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/resource.h>
 #include <sys/wait.h>
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -15,10 +18,13 @@
 void signal_handler( int );
 // 定义处理进程名称的函数
 void set_process_title();
+// daemonize函数
+void be_daemon();
 
 // 子进程数量
 int worker_num = 4;
 int port       = 6666;
+int daemonize  = 0;
 
 // 环境变量数组 
 extern char ** environ;
@@ -50,15 +56,27 @@ int main( int argc, char * argv[] ) {
   // 声明配置文件路径
   int  file_size;  // bytes
   FILE * conf_file_fp;
-  char * conf_file = "./conf/tiginx.conf";
+  char * worker_dir = NULL;
+  char * conf_file = "/conf/tiginx.conf";
+  char * full_conf_file = NULL;
   char * conf_content;
   cJSON * conf_content_root;
   // 声明配置项目相关变量
   cJSON * conf_port;
   cJSON * conf_worker_num;
+  cJSON * conf_daemonize;
+
+  // daemonize化
+  //daemonize();
   
   // 解析配置文件
-  conf_file_fp = fopen( conf_file, "r" );  
+  worker_dir = getcwd( NULL, 0 ); 
+  full_conf_file = ( char * )malloc( sizeof( worker_dir ) + sizeof( conf_file ) + 50 );
+  strcat( full_conf_file, worker_dir );
+  free( worker_dir );
+  strcat( full_conf_file, conf_file );
+  conf_file_fp = fopen( full_conf_file, "r" );  
+  free( full_conf_file );
   if ( !conf_file_fp ) {
     printf( "open file error.\n" );
     exit( -1 );
@@ -66,17 +84,22 @@ int main( int argc, char * argv[] ) {
   fseek( conf_file_fp, 0, SEEK_END ); 
   file_size = ftell( conf_file_fp );
   rewind( conf_file_fp );
-  conf_content = ( char * )malloc( 10000 );   
+  conf_content = ( char * )malloc( file_size );   
   memset( conf_content, 0, sizeof( conf_content ) );
   fread( conf_content, sizeof( char ), file_size, conf_file_fp );
-  free( conf_content );
   fclose( conf_file_fp );
   conf_content_root = cJSON_Parse( conf_content ); 
   conf_port       = cJSON_GetObjectItem( conf_content_root, "port" );
   conf_worker_num = cJSON_GetObjectItem( conf_content_root, "worker_num" );
+  conf_daemonize  = cJSON_GetObjectItem( conf_content_root, "daemonize" );
   //printf( "%d\n", conf_worker_num->valueint );   
   port       = conf_port->valueint;
   worker_num = conf_worker_num->valueint; 
+  daemonize  = conf_daemonize->valueint; 
+  free( conf_content );
+  if ( 1 == daemonize ) {
+    be_daemon();
+  }
   // 创建监听socket  
   listen_socket = socket( AF_INET, SOCK_STREAM, 0 );
   if ( -1 == listen_socket ) {
@@ -97,22 +120,6 @@ int main( int argc, char * argv[] ) {
     printf( "listen socket error.\n" );
     exit( -1 );
   } 
-  /*
-  while ( 1 ) {
-    connect_socket = accept( listen_socket, ( struct sockaddr * )&connect_socket_addr, &socket_length );
-    if ( -1 == connect_socket ) {
-      printf( "accpet socket error.\n" );
-      exit( -1 );
-    }
-    recv( connect_socket, &buffer, MAXBUFFER, 0 ); 
-    printf( "%s\n", buffer ); 
-    char msg[ MAXBUFFER ] = "hello,nihao";
-    send( connect_socket, &msg, MAXBUFFER, 0 );
-    close( connect_socket );
-  } 
-  close( listen_socket );
-  return 0;
-  */
  
   // fork子进程
   for ( int i = 0; i < worker_num; i++ ) {
@@ -174,4 +181,54 @@ void signal_handler( int signal ) {
     pid_t pid;
     pid = waitpid( -1, &waitpid_status, WNOHANG );   
   }
+}
+
+void be_daemon() {
+
+  pid_t  pid; 
+  struct sigaction sa;
+  int    i;
+  int    open_fd_max_num;
+  int    fd0, fd1, fd2;
+  char * worker_dir = NULL;
+  
+  pid = fork();    
+  if ( 0 > pid ) {
+    printf( "fork error\n" );
+    exit( -1 );
+  }  
+  else if ( 0 < pid ) {
+    exit( -1 );
+  }
+  setsid();
+  pid = fork();
+  if ( 0 > pid ) {
+    printf( "fork error\n" );
+    exit( -1 );
+  } 
+  else if ( 0 < pid ) {
+    exit( -1 );
+  } 
+  // 设置umask ， 给进程最大的创建文件夹以及文件权限  
+  umask( 0 );
+  // 将目录更改为根目录，防止挂载目录无法卸载
+  worker_dir = getcwd( NULL, 0 );
+  chdir( worker_dir );
+  free( worker_dir );
+  // 忽略sighup信号
+  sa.sa_handler = SIG_IGN;
+  sigemptyset( &sa.sa_mask );   
+  sa.sa_flags  = 0;
+  sigaction( SIGHUP, &sa, NULL ); 
+  // 关闭父进程继承过来的文件描述
+  open_fd_max_num = sysconf( _SC_OPEN_MAX ); 
+  open_fd_max_num = RLIM_INFINITY == open_fd_max_num ? 1024 : open_fd_max_num ;
+  for ( i = 0; i < open_fd_max_num; i++ ) {
+    //close( i );
+  }
+  // 重定向标准输入输出
+  //fd0 = open( "/dev/null", O_RDWR );   
+  //fd1 = dup( 0 );
+  //fd2 = dup( 0 );
+  //printf( "daemonize over\n" );
 }
